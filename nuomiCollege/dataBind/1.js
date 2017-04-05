76b2dee1c0de2b30517a0d6ade436b1efbcde2d1
Ruo Mo <damoshen@163.com>
2017-04-05 16:04:59 +0800

    数据绑定1

--- nuomiCollege/dataBind/1.js
+++ nuomiCollege/dataBind/1.js
@@ -1,5 +1,6 @@
 function Observer(object){
     var keys = Object.keys(object),
+        that = this
 
     this.data = {}
     keys.forEach(function(key){
@@ -8,17 +9,18 @@ function Observer(object){
             new Observer(val)
         }
         var privateKey = '__'  + key
-        data[privateKey] = object[key]
-        Object.defineProperty(data,key,{
+        that.data[privateKey] = object[key]
+        Object.defineProperty(that.data,key,{
+            enumerable : true,
+            configurable : true,
             get : function(){
                 console.log("你访问了" + key)
-                return data[privateKey]
+                return that.data[privateKey]
             },
             set : function(value){
-                data[privateKey] = value
+                that.data[privateKey] = value
                 console.log("你设置了" + key + "的值，新的值为" + value)
             }
         })
     })
-
 }
